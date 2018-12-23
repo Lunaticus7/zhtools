1e9cef12f2fd28fb350dc90170027c7bc8672175
Linusp <linusp1024@gmail.com>
2018-12-23 16:47:23 +0800

    fix exporting

--- zhtools/utils/__init__.py
+++ zhtools/utils/__init__.py
@@ -1,11 +1,9 @@
 from .inverted_index import InvertedIndex
 from .storage import (
     MemoryDocumentStorage,
-    RedisDocumentStorage,
 )
 
 __all__ = [
     'InvertedIndex',
     'MemoryDocumentStorage',
-    'RedisDocumentStorage',
 ]
